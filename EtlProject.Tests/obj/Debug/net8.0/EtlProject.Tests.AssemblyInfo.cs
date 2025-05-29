2174ed92472257a077defe615f91424cf5d0a34c
M3rcy <m3rcy139@gmail.com>
2025-05-29 12:21:32 +0300

    Minor fixes

--- EtlProject.Tests/obj/Debug/net8.0/EtlProject.Tests.AssemblyInfo.cs
+++ EtlProject.Tests/obj/Debug/net8.0/EtlProject.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EtlProject.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d5adbabd5c98bafc461531e9e47a6e670adae7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7b09d7e6dc835ffd8808bab9016b2e4612fbf40")]
 [assembly: System.Reflection.AssemblyProductAttribute("EtlProject.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EtlProject.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
